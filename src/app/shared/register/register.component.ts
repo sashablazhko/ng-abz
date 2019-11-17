9f51216b8cc3a0c0387fa1fc741bbda31fabf12e
sashablazhko <sashablazhko@gmail.com>
2019-11-17 10:53:05 +0200

    add style for fields errors

--- src/app/shared/register/register.component.ts
+++ src/app/shared/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    email: new FormControl(null, Validators.required),
+    phone: new FormControl(null, Validators.required),
+    position: new FormControl(null, Validators.required),
+    file: new FormControl(null, Validators.required)
+  })
+
   constructor() { }
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get phone() {
+    return this.registerForm.get('phone');
+  }
+
+  get position() {
+    return this.registerForm.get('position');
+  }
+
+  get file() {
+    return this.registerForm.get('file');
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup) {
+    (<any>Object).values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+
+      if (control.controls) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
+
+  onSubmit(e) {
+    this.markFormGroupTouched(this.registerForm)
+    console.log('e', e);
+    console.log('this.registerForm', this.registerForm);
+  }
 
   ngOnInit() {
   }
--- src/app/shared/register/register.module.ts
+++ src/app/shared/register/register.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { RegisterComponent } from './register.component';
 
 
@@ -7,7 +9,10 @@ import { RegisterComponent } from './register.component';
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgSelectModule
   ],
   exports: [RegisterComponent]
 })
